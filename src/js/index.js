48e20c77cbf412e22cd4328981819087edfb99bb
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-08-29 01:33:36 +0300

    practice

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-console.log(123);
+import '../css/style.css';
+import pokemonCardTpl from '../templates/pokemon-card.hbs';
+
+const listEl = document.querySelector('.js-list');
+const buttonEl = document.querySelector('.js-button');
+
+buttonEl.addEventListener('click', fetchPokemon)
+
+function fetchPokemon() {
+  return fetch("https://pokeapi.co/api/v2/pokemon/15")
+    .then(response => {
+      return response.json();
+    })
+    .then(renderPokemonCard)
+    .catch(error => console.log(error));
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  listEl.innerHTML = markup;
+}
