5d44d92b25ff96c727ce39bd80d5b431325a94c2
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-06 00:36:10 +0300

    add pnotify

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 import '../css/style.css';
+
+import { info } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import * as Confirm from "@pnotify/confirm";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
+
 import countryTpl from '../templates/country.hbs';
 import countriesListTpl from '../templates/countriesList';
 import debounce from 'lodash.debounce';
@@ -41,5 +48,45 @@ function renderCountriesList(country) {
   const markup = countriesListTpl(country);
   countryContainer.innerHTML = markup;
 }
+
+
+
+function click() {
+  info({
+    title: "Button Clicked",
+    text:
+      "You have clicked the button. You may now complete the process of reading the notice.",
+    modules: new Map([
+      [
+        Confirm,
+        {
+          confirm: true,
+          buttons: [
+            {
+              text: "Ok",
+              primary: true,
+              click: notice => {
+                notice.close();
+              }
+            }
+          ]
+        }
+      ]
+    ])
+  });
+}
+
+
+const App = document.getElementById("app");
+
+App.innerHTML = `
+<div class="container">
+  <h1>PNotify 5 in Vanilla ES6!</h1>
+  <button>Notify me!</button>
+</div>
+`;
+
+App.querySelector("button").addEventListener("click", click);
+
 
 
