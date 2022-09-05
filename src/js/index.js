c22b79d153ae6ff0f28429493f8dd5dfa671c157
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-06 00:58:52 +0300

    add notiflix

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,12 @@
 import '../css/style.css';
-
-import { info } from "@pnotify/core";
-import "@pnotify/core/dist/PNotify.css";
-import "@pnotify/core/dist/BrightTheme.css";
-import * as Confirm from "@pnotify/confirm";
-import "@pnotify/confirm/dist/PNotifyConfirm.css";
-
 import countryTpl from '../templates/country.hbs';
 import countriesListTpl from '../templates/countriesList';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const countryContainer = document.querySelector('.country');
 const formEl = document.querySelector('.js-form');
+
 
 formEl.addEventListener('input', debounce(onSearch, 500));
 
@@ -23,13 +18,15 @@ function onSearch(event) {
   fetchCountries(inputValue)
     .then(r => {
       if (r.length > 10) {
-        alert('type more letters');
+        Notify.info('Too many matches found. Please enter a more specific query!');
       } else if (r.length >= 2 && r.length <= 10) {
         renderCountriesList(r);
         return
       } else renderCountryCard(r);
     })
-    .catch(error => console.log(error));
+    .catch(err => {
+      Notify.failure('Oops, there is no country with that name');
+    });
 }
 
 
@@ -48,45 +45,6 @@ function renderCountriesList(country) {
   const markup = countriesListTpl(country);
   countryContainer.innerHTML = markup;
 }
-
-
-
-function click() {
-  info({
-    title: "Button Clicked",
-    text:
-      "You have clicked the button. You may now complete the process of reading the notice.",
-    modules: new Map([
-      [
-        Confirm,
-        {
-          confirm: true,
-          buttons: [
-            {
-              text: "Ok",
-              primary: true,
-              click: notice => {
-                notice.close();
-              }
-            }
-          ]
-        }
-      ]
-    ])
-  });
-}
-
-
-const App = document.getElementById("app");
-
-App.innerHTML = `
-<div class="container">
-  <h1>PNotify 5 in Vanilla ES6!</h1>
-  <button>Notify me!</button>
-</div>
-`;
-
-App.querySelector("button").addEventListener("click", click);
 
 
 
