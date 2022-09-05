6adec15667855d43ed14883e1fd015ee9219d676
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-06 00:17:57 +0300

    Добавил шаблон

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 import '../css/style.css';
 import countryTpl from '../templates/country.hbs';
+import countriesListTpl from '../templates/countriesList';
 import debounce from 'lodash.debounce';
-
-const _ = require("lodash");
 
 const countryContainer = document.querySelector('.country');
 const formEl = document.querySelector('.js-form');
@@ -12,25 +11,34 @@ formEl.addEventListener('input', debounce(onSearch, 500));
 function onSearch(event) {
   event.preventDefault();
 
-  const inputValue =
-    event.target.value;
+  const inputValue = event.target.value;
 
   fetchCountries(inputValue)
-    .then(renderCountryCard)
-    .catch(error => console.log(error))
+    .then(r => {
+      if (r.length > 10) {
+        alert('type more letters');
+      } else if (r.length >= 2 && r.length <= 10) {
+        renderCountriesList(r);
+        return
+      } else renderCountryCard(r);
+    })
+    .catch(error => console.log(error));
 }
 
 
 function fetchCountries(countryName) {
   return fetch(`https://restcountries.com/v2/name/${countryName}`)
-    .then(response => {
-    return response.json();
-  })
+    .then(response => response.json())
 }
 
 
 function renderCountryCard(country) {
   const markup = countryTpl(country);
+  countryContainer.innerHTML = markup;
+}
+
+function renderCountriesList(country) {
+  const markup = countriesListTpl(country);
   countryContainer.innerHTML = markup;
 }
 
